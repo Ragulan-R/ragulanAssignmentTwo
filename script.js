d7bfb7e22c054a0677b526b1e90d088910da1c08
ragulan <ragulanravi21@gmail.com>
2021-02-16 22:10:10 -0500

    added javascript color change to title and border change to js

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-// worked on js portion with previous partner so there will be some similarities
+// worked on hamburger js portion with previous project 1 partner (Rebecca) so there will be some similarities with the burger menu
+
+// 1. BURGER MENU JS
 
 // variable to target the burger icon
 let navBurgerMenu = document.getElementById("burgerIcon");
-
 // variable to target the uls in the nav
 let navBurgerItems = document.getElementById("burgerList");
 
 // function to add a class to make the burger visible
-
-// console.log(navBurgerMenu);
-// console.log(navBurgerItems);
-
 // add a class so the new class click on turns off an on so the list goes from block to inline
 navBurgerMenu.addEventListener("click", function () {
   navBurgerItems.classList.toggle("clickOn");
+});
+
+// 2. CHANGING THE COLOR OF THE WEBSITE LOGO ONCE CLICKED
+
+// let logoChange = document.getElementById("yellowLogo");
+// logoChange.addEventListener("click", function () {
+//   .style.color = "yellow";
+// });
+
+let logoChange = document.querySelector("h1");
+
+logoChange.addEventListener("click", () => {
+  document.getElementById("yellowLogo").style.color = "yellow";
+});
+
+// document.getElementById("yellowLogo").addEventListener(click, function(){
+
+// 3. CHANGING THE BORDER OF IMAGES IN THE GALLERY ONCE CLICKED
+
+let galleryBorder = document.getElementsByClassName("box");
+
+galleryBorder.forEach((img) => {
+  img.style.border = "solid 10px yellow";
 });
